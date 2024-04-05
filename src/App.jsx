3ea6826bbf678a1edf952ce4d09f98ffb87a62ac
sunjom <saysaysay223@naver.com>
2024-04-05 13:27:38 +0900

    feat : [FE] components 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-const reactDescriptions = ['Fundamental','Crucial','Core'];
-
-function getRandomInt(max){
-    return Math.floor(Math.random() * (max+1));
-}
-
-function Header(){
-    const description = reactDescriptions[getRandomInt(2)];
-    return(
-        <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {description} React concepts you will need for almost any app you are
-                going to build!
-            </p>
-        </header>
-    )
-}
+import componentsImg from './assets/components.png'
+import {CORE_CONCEPTS} from './data';
+import Header from './components/Header'
+import CoreConcept from './components/CoreConcept'
 function App() {
   return (
     <div>
       <Header/>
       <main>
-        <h2>Time to get started!</h2>
+          <section id="core-concepts">
+              <h2>Core Concepts</h2>
+              <ul>
+                  <CoreConcept{...CORE_CONCEPTS[0]}/>
+                  <CoreConcept{...CORE_CONCEPTS[1]}/>
+                  <CoreConcept{...CORE_CONCEPTS[2]}/>
+                  <CoreConcept{...CORE_CONCEPTS[3]}/>
+              </ul>
+          </section>
       </main>
     </div>
   );
