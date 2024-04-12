682c79a27dd5aa96997587bd70947a2555e83f78
sunjom <saysaysay223@naver.com>
2024-04-12 15:29:22 +0900

    feat : [FE] 컴포넌트 분리 심화과정.

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,17 @@
-import componentsImg from './assets/components.png'
-import {CORE_CONCEPTS} from './data';
 import Header from './components/Header/Header'
-import CoreConcept from './components/CoreConcept'
-import TabButton from './components/TabButton'
-import {useState } from 'react'
-import {EXAMPLES} from './data'
+import CoreConcepts from './components/CoreConcepts'
+import {useState, Fragment } from 'react'
+import Examples from './components/Examples'
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState('');
-    function handleSelect(selectedButton){
-        //selectedButton => 'components', 'jsx', 'props', 'state'
-        setSelectedTopic(selectedButton);
-        console.log(selectedTopic);
-    }
+
   return (
-    <div>
+    <>
       <Header/>
       <main>
-          <section id="core-concepts">
-              <h2>Core Concepts</h2>
-              <ul>
-                  {CORE_CONCEPTS.map((conceptItem)=>(<CoreConcept key={conceptItem.title}{...conceptItem}/>))}
-              </ul>
-          </section>
-          <section id="examples">
-              <h2>Examples</h2>
-              <menu>
-                  <TabButton isSelected={selectedTopic==='components'} onClick={() => {handleSelect('components')}}>Components</TabButton>
-                  <TabButton isSelected={selectedTopic==='jsx'} onClick={() => {handleSelect('jsx')}}>JSX</TabButton>
-                  <TabButton isSelected={selectedTopic==='props'} onClick={() => {handleSelect('props')}}>Props</TabButton>
-                  <TabButton isSelected={selectedTopic==='state'} onClick={() => {handleSelect('state')}}>State</TabButton>
-              </menu>
-                  {!selectedTopic ? <p>Please select a topic</p> :
-                      (
-                          <div id="tab-content">
-                              <h3>{EXAMPLES[selectedTopic].title}</h3>
-                              <p>{EXAMPLES[selectedTopic].description}</p>
-                              <pre>
-                              <code>
-                                  {EXAMPLES[selectedTopic].code}
-                              </code>
-                          </pre>
-                          </div>
-                      )}
-          </section>
+          <CoreConcepts/>
+          <Examples/>
       </main>
-    </div>
+    </>
   );
 }
 
